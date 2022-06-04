6d8794f6ef0dd8899fc5d4b713af19d961d9795d
Michal Plewa <themipsial@gmail.com>
2022-06-04 09:48:59 +0200

    init and first tests

---
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -0,0 +1,30 @@
+import { convertPLNToUSD } from '../convertPLNtoUSD';
+
+describe('ConvertPLNtoUSD', () => {
+  it('should return proper value when good input', () => {
+    expect(convertPLNToUSD(1)).toBe('$0.29');
+    expect(convertPLNToUSD(2)).toBe('$0.57');
+    expect(convertPLNToUSD(20)).toBe('$5.71');
+    expect(convertPLNToUSD(12)).toBe('$3.43');
+  });
+  it('should return NaN if value is a text', () => {
+    expect(convertPLNToUSD('6')).toBeNaN();
+    expect(convertPLNToUSD('89')).toBeNaN();
+    expect(convertPLNToUSD('abc')).toBeNaN();
+    expect(convertPLNToUSD('-20')).toBeNaN();
+  });
+  it('should return NaN if there is no value', () => {
+    expect(convertPLNToUSD('')).toBeNaN();
+  });
+  it('should return error if value is not a number or text', () => {
+    expect(convertPLNToUSD([])).toBe('Error');
+    expect(convertPLNToUSD(null)).toBe('Error');
+    expect(convertPLNToUSD({})).toBe('Error');
+    expect(convertPLNToUSD(true)).toBe('Error');
+  });
+  it('should return $0.00 if value is lesser than 0', () => {
+    expect(convertPLNToUSD(-1)).toBe('$0.00');
+    expect(convertPLNToUSD(-23)).toBe('$0.00');
+    expect(convertPLNToUSD(-7068)).toBe('$0.00');
+  });
+});
